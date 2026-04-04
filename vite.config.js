a18d504a5a1bccb571464c73b9af2d9792fe8a4c
huseyinyyilmaz <huseyin.yyilmaz005@gmail.com>
2026-04-04 23:15:58 +0300

    Fix Vercel white screen by resetting Vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/WebSitem/' : '/',
+export default defineConfig(() => ({
+  base: '/',
   plugins: [react()],
   server: {
     host: true,
